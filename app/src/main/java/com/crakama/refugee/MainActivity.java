d1832af1b898350314e1d32ec6af8cddc6fef564
crakama <crakama89@gmail.com>
2016-09-01 23:50:41 +0300

    Navigation Drawer and Menu items

--- app/src/main/java/com/crakama/refugee/MainActivity.java
+++ app/src/main/java/com/crakama/refugee/MainActivity.java
@@ -1,5 +1,6 @@
 package com.crakama.refugee;
 
+import android.content.res.Configuration;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
@@ -11,6 +12,8 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.TypedValue;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.astuetz.PagerSlidingTabStrip;
 
@@ -40,6 +43,7 @@ public class MainActivity extends AppCompatActivity {
         pager.setPageMargin(pageMargin);
         tabs.setViewPager(pager);
         tabs.setIndicatorColor(Color.parseColor("#3f51B5"));
+        initInstances();
 
 
     }
@@ -72,9 +76,79 @@ public class MainActivity extends AppCompatActivity {
 
     }//End ClassPagerAdapter class
 
+    /** CODE FOR NAVIGATION DRAWER */
+
+    private void initInstances() {
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayShowHomeEnabled(true);
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
+        drawerLayout = (DrawerLayout) findViewById(R.id.drawerLayout);
+        drawerToggle = new ActionBarDrawerToggle(MainActivity.this, drawerLayout, R.string.welcome, R.string.welcome);
+        drawerLayout.addDrawerListener(drawerToggle);
+        //Initializing NavigationView
+        navigation = (NavigationView) findViewById(R.id.navigation_view);
+        //Setting Navigation View Item Selected Listener to handle the item click of the navigation menu
+        navigation.setNavigationItemSelectedListener(new NavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(MenuItem menuItem) {
+                int id = menuItem.getItemId();
+                switch (id) {
+                    case R.id.navigation_item_1:
+                        //Do some thing here
+                        break;
+                    case R.id.navigation_item_2:
+                        //Do some thing here
+                        break;
+                    case R.id.navigation_item_3:
+                        //Do some thing here
+                        break;
+                    case R.id.navigation_item_4:
+                        //Do some thing here
+                        break;
+
+                }
+                return false;
+            }
+        });
+
+    }
 
 
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        drawerToggle.onConfigurationChanged(newConfig);
+    }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (drawerToggle.onOptionsItemSelected(item))
+            return true;
+
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        drawerToggle.syncState();
+    }
 
 
 
