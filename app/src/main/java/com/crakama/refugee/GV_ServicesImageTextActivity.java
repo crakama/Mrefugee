08df38d7532e71f5b0e6e0ea75a84e54c445db71
crakama <crakama89@gmail.com>
2016-09-02 22:49:53 +0300

    Drawables, images and titles

---
+++ app/src/main/java/com/crakama/refugee/GV_ServicesImageTextActivity.java
@@ -0,0 +1,26 @@
+package com.crakama.refugee;
+
+import android.support.v7.app.AppCompatActivity;
+import android.widget.GridView;
+
+/**
+ * Created by cate.rakama@gmail.com on 9/2/2016.
+ */
+public class GV_ServicesImageTextActivity extends AppCompatActivity {
+
+    GridView androidGridView;
+
+    String[] gridViewString = {
+            "PROTECTION", "Child Registration", "Repatriation", "RSD", "REFERRAL", "Resettlement", } ;
+
+    int[] gridViewImageId = {
+            R.drawable.protection, R.drawable.childregistration, R.drawable.repatriation, R.drawable.RSD, R.drawable.refferal,
+            R.drawable.resettlement,};
+
+
+
+
+}
+
+
+
